5946cf56237534b29b267afe4862b7d590adc3b7
253490962@qq.com <253490962@qq.com>
2020-11-10 10:43:43 +0800

    1.00封版

--- BenDing.Domain/Models/Params/Resident/PrescriptionUploadParam.cs
+++ BenDing.Domain/Models/Params/Resident/PrescriptionUploadParam.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Xml.Serialization;
 
 namespace BenDing.Domain.Models.Params.Resident
-{
+{ 
     [XmlRootAttribute("ROW", IsNullable = false)]
     public class PrescriptionUploadParam
     {
